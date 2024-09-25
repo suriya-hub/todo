872c60adcda5f2eaf71017df4e4b0a33b43e27b4
suriyaB <suriyasmart619@gmail.com>
2024-09-25 19:58:44 +0530

    issue fixed

---
+++ src/screens/home.jsx
@@ -0,0 +1,128 @@
+import { useRef, useState } from 'react';
+import { Button, Text, TextInput, StyleSheet, FlatList, TouchableOpacity, View } from 'react-native'
+import uuid from 'react-native-uuid';
+
+
+export default function Home() {
+
+    const [data, setData] = useState(
+        [
+            {
+                "id": 10,
+                "label": "This is the label for item 10.\nAnd it continues\nonto a third line."
+            },
+            {
+                "id": 17,
+                "label": "Th"
+            },
+            {
+                "id": 18,
+                "label": "This is the label for item 8.\nSecond line."
+            },
+            {
+                "id": 19,
+                "label": "This is the label for item 9.\nAnother second line."
+            },
+            {
+                "id": 110,
+                "label": "This is the"
+            }
+        ]
+    );
+    const [notes, setNotes] = useState('')
+    const [idSaver, setIdSaver] = useState(undefined)
+
+    const handleAdd = () => {
+        if (notes !== '') {
+
+            if (idSaver) {
+                const updatedData = data.map(item => item.id === idSaver ? { ...item, label: notes } : item);
+                setData(updatedData);
+                setIdSaver(undefined);
+                setNotes('');
+            } else {
+                setData([...data, { id: uuid.v4(), label: notes }])
+                setNotes('')
+            }
+        }
+    }
+
+    const handleRemove = (id) => {
+        const removed = data.filter((rem) => rem.id !== id)
+        setData(removed)
+    }
+
+    const handleEdit = (text) => {
+        setNotes(text)
+    }
+
+    const handleCancel = () => {
+        setIdSaver(undefined);
+        setNotes('');
+    }
+
+
+    return (
+        <>
+            <View style={styles.screenContainer}>
+                <View>
+                    <TextInput
+                        multiline
+                        numberOfLines={4}
+                        style={styles.input}
+                        value={notes}
+                        placeholder="Type a note.."
+                        onChangeText={(text) => setNotes(text)}
+                    />
+                    <View style={styles.actionContainer}>
+                        <Button title="Add" onPress={() => handleAdd()} />
+                        <Button title="Cancel" onPress={() => handleCancel()} />
+                    </View>
+                </View>
+                <FlatList
+                    data={data}
+                    keyExtractor={(item) => item.id}
+                    initialNumToRender={data.length}
+                    renderItem={(item) => (
+                        <View style={styles.listingContainer}>
+                            <TouchableOpacity
+                                style={styles.messageList}
+                                onPress={() => [
+                                    setIdSaver(item.item.id),
+                                    handleEdit(item.item.label),
+                                ]}>
+                                <View>
+                                    <Text multiline numberOfLines={2} style={styles.message}>
+                                        {item.item.label}
+                                    </Text>
+                                </View>
+                            </TouchableOpacity>
+                            <View>
+                                <Button title="X" onPress={() => handleRemove(item.item.id)} />
+                            </View>
+                        </View>
+                    )}
+                    ItemSeparatorComponent={() => <View style={styles.seperator} />}
+                />
+            </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    screenContainer: { padding: 10, rowGap: 20, flex: 1 },
+    messageContainer: { marginTop: 30 },
+    messageList: { paddingVertical: 10, flexShrink: 1, width: '100%', minHeight: 50, },
+    messageListContainer: {},
+    input: { maxHeight: 100, backgroundColor: 'lightgrey', marginBottom: 20, lineHeight: 20 },
+    listingContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        gap: 10,
+    },
+    message: { color: 'black' },
+    seperator: { backgroundColor: 'lightgrey', height: 1 },
+    actionContainer: { rowGap: 5 },
+});
